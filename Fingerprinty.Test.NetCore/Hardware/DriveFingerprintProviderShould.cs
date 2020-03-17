c98ddf0bd69609ec2d61938f93ada74d153cb89d
melchiork <2656932+melchiork@users.noreply.github.com>
2020-03-17 21:06:14 +0100

    DriveFingerprintProvider

---
+++ Fingerprinty.Test.NetCore/Hardware/DriveFingerprintProviderShould.cs
@@ -0,0 +1,9 @@
+﻿using Fingerprinty.Hardware;
+
+namespace Fingerprinty.Test.NetCore.Hardware
+{
+    public class DriveFingerprintProviderShould : AllHardwareProvidersShould
+    {
+        protected override HardwareFingerprintProvider FingerprintProvider { get; } = new DriveFingerprintProvider();
+    }
+}
---
+++ Fingerprinty/Hardware/DriveFingerprintProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Management;
+
+namespace Fingerprinty.Hardware
+{
+    public class DriveFingerprintProvider : HardwareFingerprintProvider
+    {
+        public override HardwareFingerprint Get()
+        {
+            var cDriveSerial = GetCDriveSerial();
+
+            return FingerprintFactory.Create(cDriveSerial);
+        }
+
+        private static string GetCDriveSerial()
+        {
+            try
+            {
+                var disk = new ManagementObject(@"win32_logicaldisk.deviceid=""C:""");
+                disk.Get();
+
+                var volumeSerial = disk["VolumeSerialNumber"].ToString();
+                disk.Dispose();
+
+                return volumeSerial;
+            }
+            catch (Exception ex)
+            {
+                throw new FingerprintyException("Not possible to find disk C:.", ex);
+            }
+        }
+
+        public override SupportedPlatforms SupportedPlatforms { get; } = SupportedPlatforms.Windows;
+    }
+}
