eeee270286605b52ecc3f284ccc83d1ff7775393
melchiork <2656932+melchiork@users.noreply.github.com>
2020-03-29 01:53:06 +0100

    FingerprintProviderFactory

---
+++ Fingerprinty.Test.NetCore/FingerprintProviderFactoryShould.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Fingerprinty.Test.NetCore
+{
+    public class FingerprintProviderFactoryShould
+    {
+        [Fact]
+        public void HaveStaticDefaultProperty()
+        {
+            typeof(FingerprintProviderFactory)
+                .Should()
+                .HaveProperty<FingerprintProviderFactory>("Default")
+                .Which.GetMethod.IsStatic.Should().BeTrue();
+        }
+    }
+}
---
+++ Fingerprinty/FingerprintProviderFactory.cs
@@ -0,0 +1,14 @@
+﻿using Fingerprinty.Hardware;
+
+namespace Fingerprinty
+{
+    public class FingerprintProviderFactory
+    {
+        public static FingerprintProviderFactory Default { get; } = new FingerprintProviderFactory();
+
+        public virtual HardwareFingerprintProvider CreateDriveProvider() => new DriveFingerprintProvider();
+        public virtual HardwareFingerprintProvider CreateMacAddressProvider() => new MacAddressFingerprintProvider();
+        public virtual HardwareFingerprintProvider CreateMachineNameProvider() => new MachineNameFingerprintProvider();
+        public virtual HardwareFingerprintProvider CreateWindowsProductIdProvider() => new WindowsProductIdFingerprintProvider();
+    }
+}
