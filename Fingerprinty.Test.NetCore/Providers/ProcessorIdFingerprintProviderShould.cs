b5d915f7d3a49c27e12905892adcd067250aa72b
melchiork <2656932+melchiork@users.noreply.github.com>
2020-04-29 23:12:04 +0200

    ProcessorIdFingerprintProvider

---
+++ Fingerprinty.Test.NetCore/Providers/ProcessorIdFingerprintProviderShould.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Fingerprinty.Test.NetCore.Providers
+{
+    public class ProcessorIdFingerprintProviderShould : AllFingerprintProvidersShould
+    {
+        protected override FingerprintProvider FingerprintProvider { get; } =
+            FingerprintProviderFactory.Default.CreateProcessorIdFingerprintProvider();
+
+        [Fact]
+        public void SupportOnlyWindows()
+        {
+            FingerprintProvider.SupportedPlatforms.Should().Be(SupportedPlatforms.Windows);
+        }
+    }
+}
